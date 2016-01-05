9aa1a7563204907645c46a2a74231811b50f05b0
Thomas Bruederli <t.bruederli@nexbyte.com>
2016-01-05 15:04:57 +0100

    Allow to supply bind IP via env

--- server.js
+++ server.js
@@ -98,7 +98,7 @@ app.post('/run', function(req, res)
 
 
 process.env.EXPRESS_SERVER = true;
-var server = app.listen(process.env.PORT || 3000, function()
+var server = app.listen(process.env.PORT || 3000, process.env.IP || '0.0.0.0', function()
   {
   var host = server.address().address;
   var port = server.address().port;
